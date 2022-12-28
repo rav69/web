9527b64b32988222a14472542c829c35058d211b
rav69 <favorit-mb@yandex.ru>
2022-12-28 15:16:56 +0300

    fgsdfhfghjfgdjdfgh

---
+++ init.sh
@@ -0,0 +1,7 @@
+# # sudo unlink /etc/nginx/sites-enabled/default
+# sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+# sudo /etc/init.d/nginx restart
+
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/nginx.conf
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
