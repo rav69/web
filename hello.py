200f02a5312feb8d74789e9bd7df37ffb2b5ecd1
rav69 <favorit-mb@yandex.ru>
2022-12-29 09:19:16 +0300

    sdfsdgfdfgdfs

--- hello.py
+++ hello.py
@@ -8,4 +8,4 @@ def app(environ, start_response):
     ]
     start_response(status, response_headers)
     # return iter([data.encode('utf-8')])
-    return iter([data])
+    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')), encoding="utf8")]
--- init.sh
+++ init.sh
@@ -9,4 +9,6 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
 
 sudo /etc/init.d/gunicorn restart
-gunicorn --bind='0.0.0.0:8080' hello:app
+gunicorn --bind='http://127.0.0.1:8000' hello:app
+
+
