3f5f11c1fb9e63de5ae8d5991908d9a864b95860
rav69 <favorit-mb@yandex.ru>
2022-12-29 10:29:43 +0300

    ghjfhsdfahagfh

--- hello.py
+++ hello.py
@@ -8,4 +8,7 @@ def app(environ, start_response):
     ]
     start_response(status, response_headers)
     # return iter([data.encode('utf-8')])
-    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')), encoding="utf8")]
+    # return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')), encoding="utf8")]
+    # return [bytes(i+'\n','ascii') for i in data.split('&')]
+    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+    return body
