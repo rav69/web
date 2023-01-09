82d3d0c176491ea76abdf8dfbf130e591aba8d79
rav69 <favorit-mb@yandex.ru>
2023-01-09 11:58:14 +0300

    fgdhdfghdfghdfgh

--- init.sh
+++ init.sh
@@ -1,13 +1,9 @@
-# # sudo unlink /etc/nginx/sites-enabled/default
-# sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-# sudo /etc/init.d/nginx restart
-
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo nginx -t
 sudo /etc/init.d/nginx restart
+
 sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
 
 sudo /etc/init.d/gunicorn restart
 
-# gunicorn -c /etc/gunicorn.d/hello.py hello:app
 gunicorn --bind='0.0.0.0:8080' hello:app
