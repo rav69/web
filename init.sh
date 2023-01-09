3b014e5fe92920e0782297d93b1cacef6fb8bae6
rav69 <favorit-mb@yandex.ru>
2023-01-09 11:26:41 +0300

    sdfdsgsdfgsdfg

--- init.sh
+++ init.sh
@@ -8,4 +8,5 @@ sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
 
 sudo /etc/init.d/gunicorn restart
 
-gunicorn -c /etc/gunicorn.d/hello.py hello:app
+# gunicorn -c /etc/gunicorn.d/hello.py hello:app
+gunicorn --bind='0.0.0.0:8080' hello:app
