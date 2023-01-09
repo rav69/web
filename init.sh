844620de28f0b5fb84580e96a8aea08a85202fe5
rav69 <favorit-mb@yandex.ru>
2023-01-09 11:42:26 +0300

    rtuyjkuipol;kojh;

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@
 # sudo /etc/init.d/nginx restart
 
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo nginx -t
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
 
