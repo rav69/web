f95841b508120bd3e0ef85d358b5f9ddbecfe509
rav69 <favorit-mb@yandex.ru>
2023-01-10 16:05:05 +0300

    jgjghjghjghjghj

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # w1_8
 w1_8
+<!-- https://github.com/Padking/web.git -->
--- init.sh
+++ init.sh
@@ -7,3 +7,4 @@ sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
 
 gunicorn --bind='0.0.0.0:8080' hello:app
+gunicorn --bind='0.0.0.0:8000' hello:app
